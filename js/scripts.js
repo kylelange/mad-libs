27b84d33de6f952fa008422c1448dc51188f7690
Jena SanCartier and Kyle Lange <kyle@>
2016-08-15 11:26:35 -0700

    add a .forEach loop to make the previous code DRY

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
 
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var adjectiveInput = $("input#adjective").val();
-    var exclamationInput = $("input#exclamation").val();
-    var nounInput = $("input#noun").val();
-    var verbInput = $("input#verb").val();
+    var blanks = ["person1", "person2", "adjective", "exclamation", "noun", "verb"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".adjective").text(adjectiveInput);
-    $(".exclamation").text(exclamationInput);
-    $(".noun").text(nounInput);
-    $(".verb").text(verbInput);
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+
+    //
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var adjectiveInput = $("input#adjective").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var nounInput = $("input#noun").val();
+    // var verbInput = $("input#verb").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".adjective").text(adjectiveInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".noun").text(nounInput);
+    // $(".verb").text(verbInput);
 
     $("#story").show();
 
